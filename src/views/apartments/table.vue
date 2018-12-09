817701bc4b9948cb628b1c7d48468f985deac21c
pobx <geidtiphong@gmail.com>
2018-12-09 10:05:55 +0700

    make table and set data to table already

--- src/views/apartments/table.vue
+++ src/views/apartments/table.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="animated fadeIn">Hi from apartments table !</div>
+  <div class="animated fadeIn">
+    <b-row>
+      <b-col md="12">
+        <b-card header="Traffic &amp; Sales">
+          <b-table bordered hover responsive :items="items" :fields="fields">
+            <template slot="index" slot-scope="data">{{ data.index + 1 }}</template>
+            <template slot="edit" slot-scope="data">
+              <b-btn size="sm" variant="warning" @click="setDataToForm(data)">{{ data.field.label }}</b-btn>
+            </template>
+          </b-table>
+
+          <b-pagination size="md" :total-rows="total_rows" v-model="currentPage" :per-page="10"></b-pagination>
+        </b-card>
+      </b-col>
+    </b-row>
+  </div>
 </template>
 
 <script>
@@ -9,11 +24,13 @@ export default {
     return {
       fields: [
         // A column that needs custom formatting
-        { key: 'index', label: 'No' },
-        { key: 'name', label: 'Apartments' },
-        { key: 'status', label: 'แก้ไข' }
+        { key: "index", label: "No", sortable: true, 'class': 'text-center' },
+        { key: "name", label: "Apartments", sortable: true, 'class': 'text-center' },
+        { key: "edit", label: "แก้ไข", 'class': 'text-center' }
       ],
-      items: []
+      items: [],
+      currentPage: 1,
+      total_rows: 0
     };
   },
   created() {
@@ -23,13 +40,19 @@ export default {
     getApartments() {
       getApartments()
         .then(response => {
-          console.log(response);
           this.items = response.data;
+          this.total_rows = this.items.length;
+          console.log(this.items);
         })
         .catch(e => {
           console.log(e);
         });
+    },
+
+    setDataToForm(data) {
+      console.log(data);
     }
+    
   }
 };
 </script>
