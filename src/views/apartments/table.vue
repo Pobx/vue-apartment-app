170b75bf9cdfd62f5fa9b6f2408d1fa644ef205a
pobx <geidtiphong@gmail.com>
2018-12-09 11:06:24 +0700

    set data to fom input apartment for update already

--- src/views/apartments/table.vue
+++ src/views/apartments/table.vue
@@ -1,19 +1,42 @@
 <template>
   <div class="animated fadeIn">
-    <b-row>
-      <b-col md="12">
-        <b-card header="รายการ Apartments">
+    <b-card header="รายการ Apartments">
+      <b-row>
+        <b-col md="12">
+          <b-form inline v-on:submit="onSubmit">
+            <label class="mr-sm-2" for="inlineFormApartmentsName">Apartment</label>
+            <b-form-input
+              class="col-md-4"
+              v-model="form.name"
+              type="text"
+              id="inlineFormApartmentsName"
+              placeholder="พิมพ์ชื่อ Apartment"
+            />&nbsp;
+            <b-form-input type="text" v-model="form.id"/>
+            <b-button type="submit" class="ml-sm-2" variant="success">บันทึก</b-button>
+          </b-form>
+        </b-col>
+      </b-row>
+
+      <br>
+
+      <b-row>
+        <b-col md="12">
           <b-table bordered hover responsive :items="items" :fields="fields">
             <template slot="index" slot-scope="data">{{ data.index + 1 }}</template>
             <template slot="edit" slot-scope="data">
-              <b-btn size="sm" variant="warning" @click="setDataToForm(data)">{{ data.field.label }}</b-btn>
+              <b-btn
+                size="sm"
+                variant="warning"
+                v-on:click="setDataToForm(data)"
+              >{{ data.field.label }}</b-btn>
             </template>
           </b-table>
 
           <b-pagination size="md" :total-rows="total_rows" v-model="currentPage" :per-page="10"></b-pagination>
-        </b-card>
-      </b-col>
-    </b-row>
+        </b-col>
+      </b-row>
+    </b-card>
   </div>
 </template>
 
@@ -22,11 +45,20 @@ import { getApartments } from "@/shared/apartments-services";
 export default {
   data: () => {
     return {
+      form: {
+        id: 0,
+        name: null
+      },
       fields: [
         // A column that needs custom formatting
-        { key: "index", label: "No", sortable: true, 'class': 'text-center' },
-        { key: "name", label: "Apartments", sortable: true, 'class': 'text-center' },
-        { key: "edit", label: "แก้ไข", 'class': 'text-center' }
+        { key: "index", label: "No", sortable: true, class: "text-center" },
+        {
+          key: "name",
+          label: "Apartments",
+          sortable: true,
+          class: "text-center"
+        },
+        { key: "edit", label: "แก้ไข", class: "text-center" }
       ],
       items: [],
       currentPage: 1,
@@ -42,7 +74,7 @@ export default {
         .then(response => {
           this.items = response.data;
           this.total_rows = this.items.length;
-          console.log(this.items);
+          // console.log(this.items);
         })
         .catch(e => {
           console.log(e);
@@ -50,9 +82,15 @@ export default {
     },
 
     setDataToForm(data) {
-      console.log(data.item);
+      // console.log(data.item);
+      this.form.id = data.item.id;
+      this.form.name = data.item.name;
+    },
+
+    onSubmit(evt) {
+      evt.preventDefault();
+      alert(JSON.stringify(this.form));
     }
-    
   }
 };
 </script>
