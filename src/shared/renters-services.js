05ccb056b401163b97fce164abd39e4ce07f39c3
pobx <geidtiphong@gmail.com>
2018-12-16 09:21:25 +0700

    create component and service of renters

---
+++ src/shared/renters-services.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+import { API } from "./api-base";
+
+export function getRenters() {
+  return axios.get(`${API}/renters`);
+}
+
+export function setRenters(data) {
+  if (data.id == 0) {
+    return axios.post(`${API}/renters/create`, data);
+  } else {
+    return axios.put(`${API}/renters/update`, data);
+  }
+}
---
+++ src/views/renters/table.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="animated fadeIn">
+    <b-card header="รายการ Apartments">
+      <b-row>
+        <b-col md="12">
+          <b-form inline v-on:submit.prevent="onSubmit" autocomplete="off">
+            <label class="mr-sm-2" for="inlineFormApartmentsName">Apartment</label>
+            <b-form-input
+              class="col-md-4"
+              v-model="form.name"
+              type="text"
+              id="inlineFormApartmentsName"
+              placeholder="พิมพ์ชื่อ Apartment"
+            />&nbsp;
+            <input type="hidden" v-model="form.id"/>
+            <b-button type="submit" class="ml-sm-2" variant="success">บันทึก</b-button>
+          </b-form>
+        </b-col>
+      </b-row>
+
+      <br>
+
+      <b-row>
+        <b-col md="12">
+          <b-table 
+            bordered 
+            hover 
+            responsive=true
+            :current-page="currentPage"
+            :per-page="perPage"  
+            :items="items" 
+            :fields="fields">
+            <template slot="index" slot-scope="data">{{ data.index + 1 }}</template>
+            <template slot="edit" slot-scope="data">
+              <b-btn
+                size="sm"
+                variant="warning"
+                v-on:click="setDataToForm(data)"
+              >{{ data.field.label }}</b-btn>
+            </template>
+          </b-table>
+
+          <b-pagination size="md" :total-rows="totalRows" v-model="currentPage" :per-page="perPage"></b-pagination>
+        </b-col>
+      </b-row>
+    </b-card>
+  </div>
+</template>
+
+<script>
+import { getApartments, setApartments } from "@/shared/apartments-services";
+export default {
+  data: () => {
+    return {
+      form: {
+        id: 0,
+        name: null
+      },
+      fields: [
+        // A column that needs custom formatting
+        { key: "index", label: "#", class: "text-center" },
+        {
+          key: "name",
+          label: "Apartments",
+          sortable: true,
+          class: "text-center"
+        },
+        { key: "edit", label: "แก้ไข", class: "text-center" }
+      ],
+      items: [],
+      currentPage: 1,
+      totalRows: 0,
+      perPage: 10
+    };
+  },
+  created() {
+    this.getApartments();
+  },
+  methods: {
+    getApartments() {
+      getApartments()
+        .then(response => {
+          this.items = response.data;
+          this.totalRows = this.items.length;
+          // console.log(this.items);
+        })
+        .catch(e => console.log(e));
+    },
+
+    setDataToForm(data) {
+      // console.log(data.item);
+      this.form.id = data.item.id;
+      this.form.name = data.item.name;
+    },
+
+    onSubmit() {
+      
+      if (this.form.name == null) {
+        alert('พิมพ์ชื่อ Apartment ด้วยค่ะ');
+        return false;
+      }
+
+      setApartments(this.form)
+        .then(response => {
+          // console.log(response);
+          this.getApartments();
+          this.onReset();
+        })
+        .catch(e => {
+          this.onReset();
+          console.log(e);
+        });
+    },
+
+    onReset() {
+      this.form = {
+        id: 0,
+        name: null
+      };
+    }
+
+  }
+};
+</script>
