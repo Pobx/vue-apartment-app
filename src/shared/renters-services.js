d53d08b80bfd4c8bc3cc431ed949b395976fae2d
pobx <geidtiphong@gmail.com>
2018-12-23 13:31:19 +0700

    create method: getRenterProfileById for get by id

--- src/shared/renters-services.js
+++ src/shared/renters-services.js
@@ -5,6 +5,10 @@ export function getRenters() {
   return axios.get(`${API}/renters`);
 }
 
+export function getRenterProfileById(id) {
+  return axios.get(`${API}/renters-by-id/${id}`);
+}
+
 export function setRenters(data) {
   if (data.id == 0) {
     return axios.post(`${API}/renters/create`, data);
