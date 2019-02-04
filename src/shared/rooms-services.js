925344310aab763c557fad9410f1054bd4722438
pobx <geidtiphong@gmail.com>
2019-02-04 19:34:27 +0700

    create method: getRoomsByApartmentsId

--- src/shared/rooms-services.js
+++ src/shared/rooms-services.js
@@ -5,6 +5,11 @@ export function getRooms() {
   return axios.get(`${API}/rooms`);
 }
 
+export function getRoomsByApartmentsId(id = null) {
+  return axios.get(`${API}/rooms-by-apartment-id/${id}`);
+  
+}
+
 export function setRooms(data) {
   if (data.id == 0) {
     return axios.post(`${API}/rooms/create`, data);
--- src/views/utilities-monthly-usages/table.vue
+++ src/views/utilities-monthly-usages/table.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="animated fadeIn">
-   APARTMENTS
+    <b-card no-body>
+      <b-tabs card v-model="tabIndex">
+        <b-tab v-for="(apartment, index) in apartments" :key="index" :title="apartment.name" @click="getRooms(apartment.id)">Tab Contents 1</b-tab>
+      </b-tabs>
+    </b-card>
   </div>
 </template>
 
@@ -9,22 +13,35 @@ import { getApartments, setApartments } from "@/shared/apartments-services";
 export default {
   data: () => {
     return {
-    }
+      tabIndex: 0,
+      apartments: []
+    };
   },
   created() {
     this.getApartments();
+    this.getRooms(1);
   },
   methods: {
     getApartments() {
       getApartments()
         .then(response => {
-          this.items = response.data;
-          this.totalRows = this.items.length;
+          this.apartments = response.data;
+          console.log(this.apartments);
+          this.totalRows = this.apartments.length;
         })
         .catch(e => this.showNotifications({ message: e }));
     },
 
-    
+    getRooms(id = null) {
+      console.log(id);
+    }
+  },
+  notifications: {
+    showNotifications: {
+      title: "ระบบแจ้งเตือน",
+      message: "เกิดข้อผิดพลาด...โปรดติดต่อผู้ดูแลระบบ",
+      type: "error"
+    }
   }
 };
 </script>
